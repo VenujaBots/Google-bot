2d12b6e7dbd0c1fdf1511219b8d099be8cc201aa
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-09 11:45:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot By @Venuja_Sadew \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ],               [InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]   ]  ) )
+	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am Vnd Google Translater Bot By @Venuja_Sadew \n I can translate any language to you selected language \n Send Your translate To me like normal Massage \n Then you can Translate__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Support™" ,url="https://t.me/vndtranslatebotsupport") ],               [InlineKeyboardButton("Subscribe 🧐", url="https://www.youtube.com/channel/UCL8PI42TZ_uaQWVVKUJx9Eg") ]   ]  ) )
                   
 
 
